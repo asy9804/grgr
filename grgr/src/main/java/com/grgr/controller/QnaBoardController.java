eb80e6782ce03c0fec183159df466d8b3bfd9cca
asy98 <asy98@LAPTOP-8VS1EDCJ>
2023-08-22 17:45:18 +0900

    0822

--- grgr/src/main/java/com/grgr/controller/QnaBoardController.java
+++ grgr/src/main/java/com/grgr/controller/QnaBoardController.java
@@ -1,17 +1,9 @@
 package com.grgr.controller;
 
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.util.HtmlUtils;
 
-import com.grgr.dto.QnaBoard;
 import com.grgr.service.QnaBoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,20 +14,13 @@ import lombok.RequiredArgsConstructor;
 public class QnaBoardController {
 	private final QnaBoardService qnaBoardService;
 	
-	//게시글 목록 검색 >> GET 요청
-	@GetMapping("/board_list")
-	@ResponseBody
-	public Map<String, Object> qnaBoardList(@RequestParam(defaultValue = "1") int pageNum) {
-		return qnaBoardService.getQnaBoardList(pageNum);
+	//게시글 목록 요청
+	@RequestMapping("/board_list")
+	public String qnaBoardList() {
+		return "/board_list";
 	}
 	
-	//게시글 삽입 처리 >> POST 요청
-	@PostMapping("/board_add")
-	public String qnaBoardAdd(@RequestBody QnaBoard qnaBoard) {
-		qnaBoard.setQna_content(HtmlUtils.htmlEscape(qnaBoard.getQna_content()));
-		qnaBoardService.addQnaBoard(qnaBoard);
-		return "success";
-	}
+	
 	
 	
 }
