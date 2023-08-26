a09ee612b830430ca847f8a64e4ac1a09222c3d8
asy98 <asy98@LAPTOP-8VS1EDCJ>
2023-08-26 20:38:40 +0900

    타일 수정

--- grgr/src/main/java/com/grgr/controller/TilesController.java
+++ grgr/src/main/java/com/grgr/controller/TilesController.java
@@ -9,29 +9,4 @@ public class TilesController {
 	public String main() {
 		return "main";
 	}
-	
-	@RequestMapping("/footer")
-	public String footer() {
-		return "footer";
-	}
-	
-	@RequestMapping("/tiles2")
-	public String tiles2() {
-		return "layout/sub/tiles2";
-	}
-	
-	@RequestMapping("/admin")
-	public String admin() {
-		return "admin";
-	}
-	
-	@RequestMapping("/admin1")
-	public String admin1() {
-		return "admin/display1";
-	}
-	
-	@RequestMapping("/admin2")
-	public String admin2() {
-		return "admin/sub/display2";
-	}
 }
